6d471e881fcaff5770cfdebdc1ff803ce6177908
Soyeon Lee <pink731p@gmail.com>
2024-11-20 14:51:21 +0900

    feat: 인터페이스, 타입 지정

---
+++ enums.ts
@@ -0,0 +1,8 @@
+// 게임 진행 상태
+export enum GameState {
+    startGame = '1',
+    showGameRecords = '2',
+    showGameStatistics = '3',
+    endGame = '9',
+    runningGame = 'RUNNING',
+}
---
+++ types.ts
@@ -0,0 +1,4 @@
+// BallNumber 타입 선언
+export type BallNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+export const ballNumberArray: BallNumber[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
